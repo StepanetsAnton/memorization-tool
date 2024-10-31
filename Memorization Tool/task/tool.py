e43bcefbea15ad836290c8ebe3545324a5ae396c
anton_stepanets <anton.stepanets15@gmail.com>
2024-10-31 20:30:37 +0100

    sqlalchemy database

--- Memorization Tool/task/tool.py
+++ Memorization Tool/task/tool.py
@@ -1,6 +1,24 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+engine = create_engine("sqlite:///flashcard.db", echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Flashcard(Base):
+    __tablename__ = 'flashcards'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    question = Column(String, nullable=False)
+    answer = Column(String, nullable=False)
+
+Base.metadata.create_all(engine)
+
 class FlashcardApp:
     def __init__(self):
-        self.flashcards = []
+        self.session = session
 
     def main_menu(self):
         while True:
@@ -35,7 +53,9 @@ class FlashcardApp:
                 while answer == "":
                     answer = input("Answer:\n").strip()
 
-                self.flashcards.append((question, answer))
+                new_flashcard = Flashcard(question=question, answer=answer)
+                self.session.add(new_flashcard)
+                self.session.commit()
 
             elif choice == "2":
                 break
@@ -43,16 +63,17 @@ class FlashcardApp:
                 print(f"{choice} is not an option")
 
     def practice_flashcards(self):
-        if not self.flashcards:
+        flashcards = self.session.query(Flashcard).all()
+        if not flashcards:
             print("\nThere is no flashcard to practice!")
             return
 
-        for question, answer in self.flashcards:
-            print(f"\nQuestion: {question}")
+        for flashcard in flashcards:
+            print(f"\nQuestion: {flashcard.question}")
             choice = input("Please press \"y\" to see the answer or press \"n\" to skip:\n")
 
             if choice == "y":
-                print(f"Answer: {answer}")
+                print(f"Answer: {flashcard.answer}")
             elif choice == "n":
                 continue
             else:
